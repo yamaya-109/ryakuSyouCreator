013416d51b6e79554b00ee9e81d0d38762401b1d
yamaya-109 <125778080+yamaya-109@users.noreply.github.com>
2024-07-22 14:00:09 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const Home = () => {
     try {
       const result = await createFullName(userInput);
       setFullNames(result);
-      setOutput(prevOutput => [...prevOutput, `> ${command}`, ...result.map(item => `${item.SeishikiName}: ${item.explanation}`)]);
+      setOutput(prevOutput => [...prevOutput, `> ${command}`, ...result.map(item => `${item.Tip}: ${item.SeishikiName} - ${item.explanation}`)]);
     } catch (error) {
       console.error('Error creating full name:', error);
       setOutput(prevOutput => [...prevOutput, `> ${command}`, 'Error creating full name']);
@@ -50,6 +50,8 @@ const Home = () => {
       <form onSubmit={handleSubmit}>
         <input
           type="text"
+          id="commandInput"
+          name="commandInput"
           value={command}
           onChange={(e) => setCommand(e.target.value)}
           onKeyDown={handleCommand}
